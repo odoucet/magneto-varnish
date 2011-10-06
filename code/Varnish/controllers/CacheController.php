cd7a3f76d2e19e0663667483b39edd95c4b3f048
Fabrizio Branca <github@fabrizio-branca.de>
2011-10-06 14:56:24 +0200

    Display an error message if an error has occured while purging

--- code/Varnish/controllers/CacheController.php
+++ code/Varnish/controllers/CacheController.php
@@ -9,11 +9,15 @@ class Magneto_Varnish_CacheController extends Mage_Adminhtml_CacheController {
     public function massRefreshAction(){
         // Handle varnish type
         $types = $this->getRequest()->getParam('types');
-        
+
         if (Mage::app()->useCache('varnish') ) {
-            if( (is_array($types) && in_array('varnish', $types)) || $types="varnish") { 
-                Mage::helper('varnish')->purgeAll();
-                $this->_getSession()->addSuccess(Mage::helper('adminhtml')->__("Varnish cache type purged ."));
+            if( (is_array($types) && in_array('varnish', $types)) || $types="varnish") {
+                $errors = Mage::helper('varnish')->purgeAll();
+                if (count($errors) > 0) {
+					$this->_getSession()->addError(Mage::helper('adminhtml')->__("Error while purging Varnish cache:<br />" . implode('<br />', $errors)));
+                } else {
+                	$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__("Varnish cache type purged." . var_export($errors, 1)));
+                }
             }
         }
 
