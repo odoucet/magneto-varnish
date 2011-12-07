1c39dd6af0ee77893d168b6c138a6630b39ad7db
Fabrizio Branca <github@fabrizio-branca.de>
2011-12-07 16:32:50 +0100

    Added interface for manually purging urls in the cache management

--- code/Varnish/controllers/CacheController.php
+++ code/Varnish/controllers/CacheController.php
@@ -12,11 +12,12 @@ class Magneto_Varnish_CacheController extends Mage_Adminhtml_CacheController {
 
         if (Mage::app()->useCache('varnish') ) {
             if( (is_array($types) && in_array('varnish', $types)) || $types="varnish") {
-                $errors = Mage::helper('varnish')->purgeAll();
+				$varnishHelper = Mage::helper('varnish'); /* @var $varnishHelper Magneto_Varnish_Helper_Data */
+                $errors = $varnishHelper->purgeAll();
                 if (count($errors) > 0) {
 					$this->_getSession()->addError(Mage::helper('adminhtml')->__("Error while purging Varnish cache:<br />" . implode('<br />', $errors)));
                 } else {
-                	$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__("Varnish cache purged."));
+                	$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__("Varnish cache purged: '/.*' "));
                 }
             }
         }
@@ -24,4 +25,27 @@ class Magneto_Varnish_CacheController extends Mage_Adminhtml_CacheController {
         // Allow parrent handle core cache types
         parent::massRefreshAction();
     }
+
+	/**
+	 * Purge urls
+	 *
+	 * @return void
+	 */
+	public function purgeUrlsAction() {
+
+		$urls = $this->getRequest()->getParam('purge_urls');
+		$cleanedUrls = array();
+		foreach (explode("\n", $urls) as $url) {
+			$url = trim($url);
+			if (!empty($url)) {
+				$cleanedUrls[] = $url;
+			}
+		}
+		if (count($cleanedUrls) > 0) {
+			$varnishHelper = Mage::helper('varnish'); /* @var $varnishHelper Magneto_Varnish_Helper_Data */
+			$varnishHelper->purge($cleanedUrls);
+			$this->_getSession()->addSuccess(Mage::helper('adminhtml')->__("Purged Varnish Urls: ") . '<pre>' . implode("\n", $cleanedUrls).'</pre>');
+		}
+		$this->_redirect('*/*/index');
+	}
 }
